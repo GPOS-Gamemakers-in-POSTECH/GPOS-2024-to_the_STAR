e96dca7798d527e0c7ce83b26a91a3c04098dc1b
bjr7000 <>
2024-08-02 16:28:34 +0900

    Feat: 드론 기초 변수 및 추상함수 구현

--- to_the_STAR/Assets/Game/Scripts/Enemy/Drone.cs
+++ to_the_STAR/Assets/Game/Scripts/Enemy/Drone.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Drone : MonoBehaviour, EnemyInterface
 {
-    // Start is called before the first frame update
+    [SerializeField] private int floor = 1; //¹Ù´Ú ±âÁØ, 0: up, 1: right, 2: down, 3: left
+    float hp = 100;
+    float attackPower = 100;
+    float detectionRange = 5;
+    float speed = 5;
+
+    int direction = 0;
+    Vector2 moveVector;
+
+    public void getDamage(float damage)
+    {
+        hp -= damage;
+        return;
+    }
+    public float attack()
+    {
+        return attackPower;
+    }
     void Start()
     {
-        
+        transform.rotation = Quaternion.Euler(0, 0, 90 * floor);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
