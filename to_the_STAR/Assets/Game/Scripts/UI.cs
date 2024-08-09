09b9fd78e7c6acbfff5d9b75121a6cc2c0e6ee3e
bjr7000 <>
2024-08-09 17:23:14 +0900

    Fix: UI 위치 오류 해결

--- to_the_STAR/Assets/Game/Scripts/UI.cs
+++ to_the_STAR/Assets/Game/Scripts/UI.cs
@@ -16,9 +16,11 @@ public class UI : MonoBehaviour
     Image Weapon_Flamethrower_Base;
     Image Weapon_Flamethrower;
 
-    float WeaponUI_y = 0.25f;
-    float WeaponUI_x_gap = 1.25f;
-    Vector2 WeaponUI_size = new Vector2(1f, 1f);
+    const float WeaponUI_y = -64f;
+    const float WeaponUI_x = 72f;
+    const float WeaponUI_x_gap = 36f;
+    Vector2 WeaponUI_size = new Vector2(1.5f, 1.5f);
+    Vector2 ScreenSize = new Vector2(-Screen.width / 2, Screen.height / 2);
 
     void Start()
     {
@@ -28,28 +30,28 @@ public class UI : MonoBehaviour
         Weapon_Hammer_Base.transform.SetParent(transform);
         Weapon_Hammer_Base.sprite = Weapon_Base_Prefab.sprite;
         Weapon_Hammer_Base.material = Weapon_Base_Prefab.material;
-        Weapon_Hammer_Base.GetComponent<RectTransform>().anchoredPosition = new Vector2(WeaponUI_x_gap, WeaponUI_y);
+        Weapon_Hammer_Base.GetComponent<RectTransform>().anchoredPosition = ScreenSize + new Vector2(WeaponUI_x, WeaponUI_y);
         Weapon_Hammer_Base.GetComponent<RectTransform>().sizeDelta = WeaponUI_size;
 
         Weapon_Hammer = new GameObject("UI_Hammer").AddComponent<Image>();
         Weapon_Hammer.transform.SetParent(transform);
         Weapon_Hammer.sprite = Weapon_Hammer_Prefab.sprite;
         Weapon_Hammer.material = Weapon_Hammer_Prefab.material;
-        Weapon_Hammer.GetComponent<RectTransform>().anchoredPosition = new Vector2(WeaponUI_x_gap, WeaponUI_y);
+        Weapon_Hammer.GetComponent<RectTransform>().anchoredPosition = ScreenSize + new Vector2(WeaponUI_x, WeaponUI_y);
         Weapon_Hammer.GetComponent<RectTransform>().sizeDelta = WeaponUI_size;
 
         Weapon_Flamethrower_Base = new GameObject("UI_Flamethrower_Base").AddComponent<Image>();
         Weapon_Flamethrower_Base.transform.SetParent(transform);
         Weapon_Flamethrower_Base.sprite = Weapon_Base_Prefab.sprite;
         Weapon_Flamethrower_Base.material = Weapon_Base_Prefab.material;
-        Weapon_Flamethrower_Base.GetComponent<RectTransform>().anchoredPosition = new Vector2(WeaponUI_x_gap * 2, WeaponUI_y);
+        Weapon_Flamethrower_Base.GetComponent<RectTransform>().anchoredPosition = ScreenSize + new Vector2(WeaponUI_x * 2 + WeaponUI_x_gap, WeaponUI_y);
         Weapon_Flamethrower_Base.GetComponent<RectTransform>().sizeDelta = WeaponUI_size;
 
         Weapon_Flamethrower = new GameObject("UI_Flamethrower").AddComponent<Image>();
         Weapon_Flamethrower.transform.SetParent(transform);
         Weapon_Flamethrower.sprite = Weapon_Flamethrower_Prefab.sprite;
         Weapon_Flamethrower.material = Weapon_Flamethrower_Prefab.material;
-        Weapon_Flamethrower.GetComponent<RectTransform>().anchoredPosition = new Vector2(WeaponUI_x_gap * 2, WeaponUI_y);
+        Weapon_Flamethrower.GetComponent<RectTransform>().anchoredPosition = ScreenSize + new Vector2(WeaponUI_x * 2 + WeaponUI_x_gap, WeaponUI_y);
         Weapon_Flamethrower.GetComponent<RectTransform>().sizeDelta = WeaponUI_size;
     }
 
