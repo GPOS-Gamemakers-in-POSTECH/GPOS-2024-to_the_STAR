88a6ed979c20f730910730176bc524101beb3be7
bjr7000 <>
2024-08-16 16:40:40 +0900

    Feat: 모서리에서 플레이어의 회전

--- to_the_STAR/Assets/Game/Scripts/Player/PlayerRotateTrigger.cs
+++ to_the_STAR/Assets/Game/Scripts/Player/PlayerRotateTrigger.cs
@@ -4,51 +4,60 @@ namespace Game.Player
 {
     public class PlayerRotateTrigger : MonoBehaviour
     {
+        Quaternion Up = Quaternion.Euler(new Vector3(0, 0, 180));
+        Quaternion Down = Quaternion.Euler(new Vector3(0, 0, 0));
+        Quaternion Right = Quaternion.Euler(new Vector3(0, 0, 90));
+        Quaternion Left = Quaternion.Euler(new Vector3(0, 0, 270));
+
+        private void Rotate(Quaternion to)
+        {
+            transform.rotation = to;
+        }
         private void OnTriggerEnter2D(Collider2D other)
         {
             if(other.gameObject.tag == "TurningPoint")
             {
                 int turningType = other.gameObject.GetComponent<TurningPointSet>().getType();
                 PlayerRotateDirection floor = GetComponent<PlayerData>().RotateDir;
-                Debug.Log(turningType);
+                if(turningType != 0 ) transform.position = other.gameObject.transform.position;
                 if(floor == PlayerRotateDirection.Up)
                 {
                     switch (turningType)
                     {
-                        case 3: break;
-                        case 4: break;
-                        case 5: break;
-                        case 6: break;
+                        case 3: Rotate(Right); break;
+                        case 4: Rotate(Left); break;
+                        case 5: Rotate(Left); break;
+                        case 6: Rotate(Right); break;
                     }
                 }
-                if(floor == PlayerRotateDirection.Right)
+                else if(floor == PlayerRotateDirection.Right)
                 {
                     switch (turningType)
                     {
-                        case 1: break;
-                        case 3: break;
-                        case 6: break;
-                        case 8: break;
+                        case 1: Rotate(Down); break;
+                        case 3: Rotate(Up); break;
+                        case 6: Rotate(Up); break;
+                        case 8: Rotate(Down); break;
                     }
                 }
-                if(floor == PlayerRotateDirection.Down)
+                else if(floor == PlayerRotateDirection.Down)
                 {
                     switch (turningType)
                     {
-                        case 1: break;
-                        case 2: break;
-                        case 7: break;
-                        case 8: break;
+                        case 1: Rotate(Right); break;
+                        case 2: Rotate(Left); break;
+                        case 7: Rotate(Left); break;
+                        case 8: Rotate(Right); break;
                     }
                 }
-                if(floor == PlayerRotateDirection.Left) 
+                else if(floor == PlayerRotateDirection.Left) 
                 {
                     switch (turningType)
                     {
-                        case 2: break;
-                        case 4: break;
-                        case 5: break;
-                        case 7: break;
+                        case 2: Rotate(Down); break;
+                        case 4: Rotate(Up); break;
+                        case 5: Rotate(Up); break;
+                        case 7: Rotate(Down); break;
                     }
                 }
             }
