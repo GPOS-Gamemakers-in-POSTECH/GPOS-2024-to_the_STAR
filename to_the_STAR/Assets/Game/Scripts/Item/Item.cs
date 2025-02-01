33acced946e525d107e86fa3d8d55072919fe8b8
2nter21 <155376343+2nter21@users.noreply.github.com>
2025-02-02 01:06:03 +0900

    [Feat] Fix pauseUI, Implement Item simply
    - Fix pauseMenuUI to look naturally
    - Make sample item sprite and Item script
    - Item checks OnTriggerEnter by Player, and can be collected using E

---
+++ to_the_STAR/Assets/Game/Scripts/Item/Item.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Item : MonoBehaviour
+{
+    public int itemId;
+    //public GameObject popupUI;
+    private bool isPlayerNearby = false;
+
+    void Start()
+    {
+        //popupUI.SetActive(false);
+
+        if(PlayerPrefs.GetInt("Item_" + itemId, 0) == 1)
+        {
+            //Destroy(GameObject);
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if(isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+            CollectItem();        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //popupUI.SetActive(true);
+            Debug.Log("Player is nearby Item #" + itemId);
+            //popupUI.GetComponentInChildren<TextMeshProUGUI>().text = $"[E] #{itemID} 아이템";
+            isPlayerNearby = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //popupUI.SetActive(false);            
+            isPlayerNearby = false;
+        }
+    }
+
+    void CollectItem()
+    {
+        PlayerPrefs.SetInt("Item_" + itemId, 1);
+        //FindObjectOfType<ItemUI>().ShowCollectedMessage(itemID);
+        Debug.Log("You Got Item #" + itemId);
+        //Destroy(gameObject);
+        gameObject.SetActive(false);
+    }    
+}
