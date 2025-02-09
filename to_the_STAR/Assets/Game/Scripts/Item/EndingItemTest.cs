5f623a2ac9dba60c294de04d22541f2443841e4b
jjong22 <jjong13971397@gmail.com>
2025-02-09 20:01:35 +0900

    Feat: Howtoplay 이미지 교체, 엔딩 아이템 수 표시 추가

--- to_the_STAR/Assets/Game/Scripts/Item/EndingItemTest.cs
+++ to_the_STAR/Assets/Game/Scripts/Item/EndingItemTest.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndingItemTest : MonoBehaviour
 {
+    public TextMeshProUGUI text;// 텍스트를 받아올 부분
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(ItemStatus.itemCount + " / " + ItemStatus.totalItemCount + " items collected!");
+        text.text = ItemStatus.itemCount + " / " + ItemStatus.totalItemCount + " items collected!";
     }
 
     // Update is called once per frame
